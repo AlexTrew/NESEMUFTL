ba253eb1a044fc6ab4c81bfa0ed650e9214bf1a3
alex <alex@carlos-2>
2025-07-11 17:52:46 +0200

    project init

---
+++ main.c
@@ -0,0 +1,16 @@
+# include "cpu.h"
+# include "ppu.h"
+
+# include <stdio.h>
+# include "SDL3/SDL.h"
+
+/*
+  nes emulator for the lols
+ */
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+int main(){
+  return 0;
+}
