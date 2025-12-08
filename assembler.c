e190996d1624602086f35f461a811a814e6f037d
alextrew <alextrew@protonmail.com>
2025-12-08 21:07:06 +0100

    assembler handles all addressing modes

--- assembler.c
+++ assembler.c
@@ -25,7 +25,22 @@ static uint8_t convert_instruction_string_to_opcode(GHashTable* lookup, const ch
     return *(uint8_t*)res;
 }  
 
-static void write_abs_instruction(CpuState* cpu, GHashTable* opcode_lookup, const  char* instruction, uint16_t operand) {
+static void write_single_byte_instruction(CpuState* cpu, GHashTable* opcode_lookup, const  char* instruction) {
+  uint8_t opcode = convert_instruction_string_to_opcode(opcode_lookup, instruction);
+  cpu->bus[cpu->pc] = opcode;
+  printf("wrote %x to %04x\n", cpu->bus[cpu->pc],  cpu->pc);
+  cpu->pc+=opcode_x_cpu_instruction_lookup[opcode].size_in_bytes;
+}  
+
+static void write_2_byte_instruction(CpuState* cpu, GHashTable* opcode_lookup, const  char* instruction, uint8_t operand) {
+  uint8_t opcode = convert_instruction_string_to_opcode(opcode_lookup, instruction);
+  cpu->bus[cpu->pc] = opcode;
+  cpu->bus[cpu->pc+1] = operand;
+  printf("wrote %x %x to %04x\n", cpu->bus[cpu->pc], cpu->bus[cpu->pc+1], cpu->pc);
+  cpu->pc+=opcode_x_cpu_instruction_lookup[opcode].size_in_bytes;
+}  
+
+static void write_3_byte_instruction(CpuState* cpu, GHashTable* opcode_lookup, const  char* instruction, uint16_t operand) {
   uint8_t opcode = convert_instruction_string_to_opcode(opcode_lookup, instruction);
 
   /* Here we convert the 16 bit operand into two bytes in the little endian
@@ -40,13 +55,6 @@ format.
 }  
 
 
-static void write_zp_instruction(CpuState* cpu, GHashTable* opcode_lookup, const  char* instruction, uint8_t operand) {
-  uint8_t opcode = convert_instruction_string_to_opcode(opcode_lookup, instruction);
-  cpu->bus[cpu->pc] = opcode;
-  cpu->bus[cpu->pc+1] = operand;
-  printf("wrote %x %x to %04x\n", cpu->bus[cpu->pc], cpu->bus[cpu->pc+1], cpu->pc);
-  cpu->pc+=opcode_x_cpu_instruction_lookup[opcode].size_in_bytes;
-}  
 
 void assemble(CpuState* cpu, const char* filename) {
 
@@ -72,14 +80,18 @@ void assemble(CpuState* cpu, const char* filename) {
   regex_t zp_regex;
   regex_t zp_xy_regex;
   regex_t ind_regex;
+  regex_t ind_x_regex;
+  regex_t ind_y_regex;
   regex_t imm_regex;
   regex_t implied_regex;
 
   regcomp(&abs_regex, "^[A-Z]\{3\} [\$][[:xdigit:]]\{4\}\s*;*.*$", REG_EXTENDED);
   regcomp(&abs_xy_regex, "^[A-Z]\{3\} [\$][[:xdigit:]]\{4\},[XY]\s*;*.*$", REG_EXTENDED);
   regcomp(&zp_regex, "^[A-Z]\{3\} [\$][[:xdigit:]]\{2\}\s*;*.*$", REG_EXTENDED);
-  regcomp(&zp_xy_regex, "^[A-Z]\{3\} [\$][[:xdigit:]]\{2\},[XY];\s**.*$", REG_EXTENDED);
+  regcomp(&zp_xy_regex, "^[A-Z]\{3\} [\$][[:xdigit:]]\{2\},[XY]\s*;*.*$", REG_EXTENDED);
   regcomp(&ind_regex, "^[A-Z]\{3\} ([\$][[:xdigit:]]\{4\})\s*;*.*$", REG_EXTENDED);
+  regcomp(&ind_x_regex, "^[A-Z]\{3\} ([\$][[:xdigit:]]\{2\},X)\s*;*.*$", REG_EXTENDED);
+  regcomp(&ind_y_regex, "^[A-Z]\{3\} ([\$][[:xdigit:]]\{2\}),Y\s*;*.*$", REG_EXTENDED);
   regcomp(&imm_regex, "^[A-Z]\{3\} #[\$][[:xdigit:]]\{2\}\s*;*.*$", REG_EXTENDED);
   regcomp(&implied_regex, "^[A-Z]\{3\}\s*;*.*$", REG_EXTENDED);
 
@@ -92,7 +104,7 @@ void assemble(CpuState* cpu, const char* filename) {
     if (!regexec(&abs_regex, line_buf, 0, NULL, 0)) {
       sscanf(line_buf, "%s $%hx", instruction, &operand);
       strcat(instruction, "-ABS");
-      write_abs_instruction(cpu, opcode_lookup, instruction, operand);
+      write_3_byte_instruction(cpu, opcode_lookup, instruction, operand);
     } 
 
     else if (!regexec(&abs_xy_regex, line_buf, 0, NULL, 0)) {
@@ -103,13 +115,13 @@ void assemble(CpuState* cpu, const char* filename) {
       else if (strchr(line_buf, 'X')!=NULL) {
 	strcat(instruction, "-ABS_X");
       }
-      write_abs_instruction(cpu, opcode_lookup, instruction, operand);
+      write_3_byte_instruction(cpu, opcode_lookup, instruction, operand);
     }
 
     else if (!regexec(&zp_regex, line_buf, 0, NULL, 0)) {
       sscanf(line_buf, "%s $%hx", instruction, &operand);
       strcat(instruction, "-ZP");
-      write_zp_instruction(cpu, opcode_lookup, instruction, operand & 0xFF);
+      write_2_byte_instruction(cpu, opcode_lookup, instruction, operand & 0xFF);
     }
     
     else if (!regexec(&zp_xy_regex, line_buf, 0, NULL, 0)) {
@@ -120,26 +132,39 @@ void assemble(CpuState* cpu, const char* filename) {
       else if (strchr(line_buf, 'X')!=NULL) {
 	strcat(instruction, "-ZP_X");
       }
-      write_zp_instruction(cpu, opcode_lookup, instruction, operand & 0xFF);
+      write_2_byte_instruction(cpu, opcode_lookup, instruction, operand & 0xFF);
     }
 
     else if (!regexec(&imm_regex, line_buf, 0, NULL, 0)) {
       sscanf(line_buf, "%s #$%hx", instruction, &operand);
       strcat(instruction, "-IMM");
 
-      uint8_t opcode = convert_instruction_string_to_opcode(opcode_lookup, instruction);
-      cpu->bus[cpu->pc] = opcode;
-      cpu->bus[cpu->pc+1] = operand;
-      printf("wrote %x %x to %04x\n", cpu->bus[cpu->pc], cpu->bus[cpu->pc+1], cpu->pc);
-
-      cpu->pc+=opcode_x_cpu_instruction_lookup[opcode].size_in_bytes;
-
+      write_2_byte_instruction(cpu, opcode_lookup, instruction, operand & 0xFF);
     }
     else if (!regexec(&implied_regex, line_buf, 0, NULL, 0)) {
       sscanf(line_buf, "%s", instruction);
-      printf("%s implied\n", instruction);
-    }
+      strcat(instruction, "-IMPLIED");
 
+      write_single_byte_instruction(cpu, opcode_lookup, instruction);
+    }
+    else if (!regexec(&ind_regex, line_buf, 0, NULL, 0)) {
+      sscanf(line_buf, "%s", instruction);
+      strcat(instruction, "-IND");
+
+      write_2_byte_instruction(cpu, opcode_lookup, instruction, operand & 0xFF);
+    }
+    else if (!regexec(&ind_x_regex, line_buf, 0, NULL, 0)) {
+      sscanf(line_buf, "%s", instruction);
+      strcat(instruction, "-IND_X");
+
+      write_2_byte_instruction(cpu, opcode_lookup, instruction, operand & 0xFF);
+    }
+    else if (!regexec(&ind_y_regex, line_buf, 0, NULL, 0)) {
+      sscanf(line_buf, "%s", instruction);
+      strcat(instruction, "-IND_Y");
+
+      write_2_byte_instruction(cpu, opcode_lookup, instruction, operand & 0xFF);
+    }
     else {
       printf("Syntax error in line %d\n", line_no);
       exit(1);
